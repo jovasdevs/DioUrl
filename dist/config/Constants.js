228b7613fae8fefb59cd6d7434cf5d4efe6c0bda
jovasdevs <jovasdevs@gmail.com>
2022-07-18 13:28:50 -0300

    First commit

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://encurtador-url.bmnhh.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://encurtador-url.bmnhh.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+}
